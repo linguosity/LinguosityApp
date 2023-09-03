c08b1ef6a95cd96be0236d7e7d54710049651c17
en0c-026 <bitup.games@gmail.com>
2023-09-03 20:41:16 +0200

    add: context folder

---
+++ src/context/AudioMagnament.jsx
@@ -0,0 +1,71 @@
+import React, { createContext, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import generateAudio from '../lib/generateAudio';
+
+const AudioMagnamentContext = createContext();
+
+export function AudioMagnament({ children }) {
+
+  const [audioUrl, setAudioUrl] = useState(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [voiceID, setVoiceID] = useState(null);
+  const audioPlayerRef = useRef(null);
+
+
+  useEffect(() => {
+    if (!audioUrl) {
+      return
+    }
+    handlePlay()
+
+  }, [audioUrl])
+
+  const handleGenerateAudio = async (text) => {
+    setIsGenerating(true)
+    const url = await generateAudio({ text, voiceID })
+    setAudioUrl(url)
+    setIsGenerating(false)
+  }
+
+  const handlePlay = () => {
+    if (audioPlayerRef.current) {
+      audioPlayerRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const handlePause = () => {
+    if (audioPlayerRef.current) {
+      audioPlayerRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const handleAudioEnded = () => {
+    setIsPlaying(false);
+  };
+
+  const handlePlayPauseGenerate = useCallback((story) => {
+    if (audioUrl) {
+      if (isPlaying) handlePause()
+      else handlePlay()
+    } else handleGenerateAudio(story)
+  }, [audioUrl, isPlaying])
+
+  return (
+    <AudioMagnamentContext.Provider value={{
+      audioUrl,
+      setAudioUrl,      
+      setVoiceID,
+      handlePlayPauseGenerate,
+      isGenerating,
+    }}>
+      <audio ref={audioPlayerRef} src={audioUrl} className='audio-player' onEnded={handleAudioEnded} />
+      {children}
+    </AudioMagnamentContext.Provider>
+  );
+}
+
+export function useAudioMagnament() {
+  return useContext(AudioMagnamentContext);
+}
