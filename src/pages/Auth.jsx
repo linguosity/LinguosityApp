a77454e1f4a2b6ae9a5d8a793683c03848aeb384
Brandon Brewer <Brandon@linguosity.ai>
2023-09-16 19:59:32 -0700

    saving reverted changes to this new branch

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+import { localStorageAuthKey, useFirebaseAuth } from '../context/FirebaseContext';
+import '../styles/Auth.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function Auth() {
+  const { registerUser, login, loginWithGoogle, error, loading } = useFirebaseAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const credential = localStorage.getItem(localStorageAuthKey);
+
+    if (credential) {
+      console.log('arre')
+      navigate('/app')
+    }
+  }, [])
+  
+  const handleEmailSignIn = async () => {
+    const result = await login(email, password)
+    if (result) {
+      navigate('/app')
+    }
+  };
+  
+  const handleGoogleSignIn = async () => {
+    const result = await loginWithGoogle()
+    if (result) {
+      navigate('/app')
+    }
+  };
+  
+  const handleCreateUser = async () => {
+    const result = await registerUser(email, password)
+    if (result) {
+      navigate('/app')
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-form">
+        <img src="https://uploads-ssl.webflow.com/643f1edf85eba707f45ddfc3/646255f5e004cd49868bd0df_linguosity_logo.svg" alt="Linguosity logo" />
+        <h2>Login to Linguosity</h2>
+        {error && <p className="error-message">{error}</p>}
+        <h5 className="email-heading">Email</h5>
+        <input
+          type="email"
+          placeholder=""
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <h5>Password</h5>
+        <input
+          type="password"
+          placeholder=""
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className='button-container'>
+          <button onClick={handleEmailSignIn}>Log in</button>
+        </div>
+        <div class="box">
+          <hr class="line"/>
+            <h4 className="login-with"><span>Or login with</span></h4>
+          <hr class="line"/>
+        </div>
+        <button className="google-button" onClick={handleGoogleSignIn}>
+          Sign-in with Google
+        </button>
+        <h4 className="have-account">Don't have account?</h4>
+        <div className='sign-up'>
+          <button onClick={handleCreateUser}>Sign up</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
