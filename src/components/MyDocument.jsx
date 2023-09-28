2b5893ad83db7ba740aeed33ec4c0dc3a2519867
Brandon Brewer <Brandon@linguosity.ai>
2023-09-28 11:12:21 -0700

    more aesthetic changes

--- src/components/MyDocument.jsx
+++ src/components/MyDocument.jsx
@@ -2,27 +2,51 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: {
-
+    padding: 30,
+    flexDirection: 'column',
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+    textAlign: 'center',
+  },
+  name: {
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    marginBottom: 40,
+  },
+  story: {
+    fontSize: 16,
+    marginBottom: 40,
+  },
+  question: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  answer: {
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+  },
 });
 
-export default function MyDocument({ pages }) {
+export default function MyDocument({ story, questions }) {
   return (
     <Document>
-      {
-        pages.map((text, i) => (
-          <Page key={i} size="A4" style={styles.page}>
-            <View style={styles.section}>
-              <Text>{text}</Text>
+      <Page size="A4" style={styles.page}>
+        <Text style={styles.title}>The Story of Sid</Text>
+        <Text style={styles.name}>Name: ______________________________</Text>
+        <Text style={styles.story}>{story}</Text>
+        <View>
+          {questions.map((question, index) => (
+            <View key={index}>
+              <Text style={styles.question}>{`${index + 1}. ${question}`}</Text>
+              <Text style={styles.answer}></Text>
             </View>
-          </Page>
-        ))
-      }
+          ))}
+        </View>
+      </Page>
     </Document>
-  )
+  );
 }
--- src/components/PreReading.jsx
+++ src/components/PreReading.jsx
@@ -2,45 +2,46 @@ export default function PreReading({ preReading }) {
   // Splitting by lines
   const lines = preReading.split('\n');
 
-  // Splitting anticipation guide and glossary
- // Find the indices for anticipation guide and glossary
- const anticipationGuideIndex = lines.findIndex(line => line.trim() === 'Anticipation Guide');
- const glossaryIndex = lines.findIndex(line => line.trim() === 'Glossary');
- 
-  const anticipationGuideLines = lines.slice(anticipationGuideIndex + 1, glossaryIndex);
-  const glossaryLines = lines.slice(glossaryIndex + 1);
+  // Find the indices for anticipation guide and glossary using regular expression
+  const anticipationGuideIndex = lines.findIndex(line => /^Anticipation Guide$/.test(line.trim()));
+  const glossaryIndex = lines.findIndex(line => /^Glossary$/.test(line.trim()));
 
+  // Log the found indices for debugging
+  console.log('Anticipation Guide Index:', anticipationGuideIndex);
+  console.log('Glossary Index:', glossaryIndex);
+  
+  // Extract anticipation guide and glossary lines, filtering out empty lines
+  const anticipationGuideLines = lines.slice(anticipationGuideIndex + 1, glossaryIndex).filter(line => line.trim() !== '');
+  const glossaryLines = lines.slice(glossaryIndex + 1).filter(line => line.trim() !== '');
+
+  // Process the extracted lines to get questions and glossary items
   const anticipationGuideQuestions = anticipationGuideLines.join(' ').split(/\d+\./).slice(1).map((item, index) => `${index + 1}.${item.trim()}`);
   const glossaryItems = glossaryLines.join(' ').split(/\d+\./).slice(1).map((item, index) => `${index + 1}.${item.trim()}`);
 
+  // Log the processed lines for debugging
   console.log('Anticipation Guide Lines:', anticipationGuideLines);
   console.log('Glossary Lines:', glossaryLines);
-
 
   return (
     <div id="pre-reading-window">
       {anticipationGuideQuestions.length > 0 && (
         <div>
           <h1>PRE-READING ANTICIPATION GUIDE</h1>
-          <p>
-            <ul>
-              {anticipationGuideQuestions.map((question, index) => (
-                <li key={index}>{question}</li>
-              ))}
-            </ul>
-          </p>
+          <ul>
+            {anticipationGuideQuestions.map((question, index) => (
+              <li key={index}>{question}</li>
+            ))}
+          </ul>
         </div>
       )}
       {glossaryItems.length > 0 && (
         <div>
           <h1>PRE-READING GLOSSARY</h1>
-            <p>
-              <ul>
-                {glossaryItems.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
-            </p>
+          <ul>
+            {glossaryItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
         </div>
       )}
     </div>
